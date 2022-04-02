171c5ec5ff0b0287b0f5fc0049b23b289b0b8723
Russell <ploverlake@users.noreply.github.com>
2022-04-03 01:24:07 +0800

    Refactor code style

--- src/libdispatch_semaphore_win32.c
+++ src/libdispatch_semaphore_win32.c
@@ -1,27 +1,5 @@
 /**
- *  Copyright (c) 2017, Russell
- *  All rights reserved.
- *  
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *  
- *  * Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *  
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *  
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2017, Russell. All rights reserved.
  */
 
 /*
@@ -51,41 +29,35 @@
 #include "libdispatch_semaphore_win32.h"
 #include <Windows.h>
 
-static _dispatch_thread_semaphore_t _dispatch_thread_semaphore_create(void)
-{
-    HANDLE s4;
-    s4 = CreateSemaphore(0, 0, 0x7FFFFFFF, 0);
-    return (_dispatch_thread_semaphore_t)s4;
+static _dispatch_thread_semaphore_t _dispatch_thread_semaphore_create(void) {
+  HANDLE s4;
+  s4 = CreateSemaphore(0, 0, 0x7FFFFFFF, 0);
+  return (_dispatch_thread_semaphore_t)s4;
 }
 
-static void _dispatch_thread_semaphore_dispose(_dispatch_thread_semaphore_t sema)
-{
-    HANDLE s4 = (HANDLE)sema;
-    BOOL ret = CloseHandle(s4);
+static void _dispatch_thread_semaphore_dispose(
+    _dispatch_thread_semaphore_t sema) {
+  HANDLE s4 = (HANDLE)sema;
+  BOOL ret = CloseHandle(s4);
 }
 
-void _dispatch_thread_semaphore_signal(_dispatch_thread_semaphore_t sema)
-{
-    HANDLE s4 = (HANDLE)sema;
-    BOOL ret = ReleaseSemaphore(s4, 1, NULL);
+void _dispatch_thread_semaphore_signal(_dispatch_thread_semaphore_t sema) {
+  HANDLE s4 = (HANDLE)sema;
+  BOOL ret = ReleaseSemaphore(s4, 1, NULL);
 }
 
-void _dispatch_thread_semaphore_wait(_dispatch_thread_semaphore_t sema)
-{
-    HANDLE s4 = (HANDLE)sema;
-    DWORD ret;
-    do {
-        ret = WaitForSingleObject(s4, INFINITE);
-    } while (ret != WAIT_OBJECT_0);
+void _dispatch_thread_semaphore_wait(_dispatch_thread_semaphore_t sema) {
+  HANDLE s4 = (HANDLE)sema;
+  DWORD ret;
+  do {
+    ret = WaitForSingleObject(s4, INFINITE);
+  } while (ret != WAIT_OBJECT_0);
 }
 
-_dispatch_thread_semaphore_t _dispatch_get_thread_semaphore(void)
-{
-    return _dispatch_thread_semaphore_create();
+_dispatch_thread_semaphore_t _dispatch_get_thread_semaphore(void) {
+  return _dispatch_thread_semaphore_create();
 }
 
-void _dispatch_put_thread_semaphore(_dispatch_thread_semaphore_t sema)
-{
-    _dispatch_thread_semaphore_dispose(sema);
+void _dispatch_put_thread_semaphore(_dispatch_thread_semaphore_t sema) {
+  _dispatch_thread_semaphore_dispose(sema);
 }
-
